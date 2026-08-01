e70cc9ed16c76cb83cee9d2339a8a8a65a43f73a
Sil van Diepen <me@sil.mt>
2026-08-01 11:10:45 +0200

    feat(coloring): configure ColoringCore targets

---
+++ engines/coloring/coloring-core/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+}
+
+group = "org.tiko.coloring"
+version = "0.1.0"
+
+kotlin {
+    jvm()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    targets.withType<KotlinNativeTarget>().configureEach {
+        binaries.framework {
+            baseName = "ColoringCore"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
